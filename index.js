db0ed6f99dd7b4c83d78424d285348ed637fe385
Анастасия <anastasija.nk@yandex.ru>
2024-09-25 13:57:34 +0300

    Вывод таблицы со сделками

--- index.js
+++ index.js
@@ -7,22 +7,23 @@
 //         console.log(response)
 //     );
 
-
+const bearerToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQwOWFjOTg1OTVlNjRhOGM4MTg0ZmFiZTVkNzM5YzU4ZTY4YmNhNzI5Mjc5NzM0MGRlYmZlN2VkZGZlNmE2MzA1Y2Y4ZmE3YzM3MWViZDJmIn0.eyJhdWQiOiIzOTIwOGQzMS0wZjA5LTRlYzAtYTI4Yi01NWNmZmVjMDJmYzEiLCJqdGkiOiJkMDlhYzk4NTk1ZTY0YThjODE4NGZhYmU1ZDczOWM1OGU2OGJjYTcyOTI3OTczNDBkZWJmZTdlZGRmZTZhNjMwNWNmOGZhN2MzNzFlYmQyZiIsImlhdCI6MTcyNzI1NjYxOSwibmJmIjoxNzI3MjU2NjE5LCJleHAiOjE3MjczNDMwMTksInN1YiI6IjExNTY1MjQyIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxOTcxMDU4LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiYTI2NGRkM2MtZmNjNy00ODlhLWIzNDUtNzJmNjZiYWNlMmU5IiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.WIYkpwRRpIEMKJ_cxJjJ2GeG3zCNjoFanvJ-94VtS-DZC0DHwTAObkMRuTjVs91i_zRVfGO2kZPU2vlP-koardPGKgw4FtbrkoUSzfqipEkTKqmQ6VP-u5acvaRoJxAM5acd50dg0sRWjlen5uJ_kTURNM62kdixNBq2-w2SR1goPe1hc5LGwzfNSRqJjDHkOEN5ev9iMJAnUa_LIlL-0Iq2RDL5aiUeefFOU4hyfnhgi3w2VmFkGcTFsNpTHTNkdYfGDEBFMXYG_G_vanWFsDYIzIJ2gqg1aZsMh9mnDs6iISVnwvMdaTJk-e1mgAAg0ivSbbCG3nHvprUvdVk35g";
 let xhr = new XMLHttpRequest();
-
-// 2. Настраиваем его: GET-запрос по URL /article/.../load
 xhr.open('GET', 'https://anastasijank.amocrm.ru/api/v4/leads');
-
-// 3. Отсылаем запрос
+xhr.setRequestHeader("Authorization", `Bearer ${bearerToken}`);
 xhr.send();
-
-// Устанавливаем желаемый тип ответа // По умолчанию это строка
-//xhr.responseType = 'json';
-
-// 4. Этот код сработает после того, как мы получим ответ сервера
+xhr.responseType = 'json';
 xhr.onload = function() {
     if (xhr.status == 200) {
-        console.log(xhr.response);
+        const leads = xhr.response._embedded.leads;
+        if(!leads) return;
+        for(let lead of leads) {
+            const leadRow = document.createElement('tr');
+            leadRow.innerHTML = '<td>' + lead.id + '</td>' +
+                '<td>' + lead.name + '</td>' +
+                '<td>' + lead.price.toLocaleString() + '</td>';
+            document.getElementById('lead_table').querySelector('tbody').append(leadRow);
+        }
     }
 };
 
